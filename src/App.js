82b25e75c5bd6e56e033e18f4874c4c66f7be1d6
Muhammad Haris <muhammadharisashfaq@gmail.com>
2022-07-27 23:41:59 +0500

    fetch Alchemy api

--- src/App.js
+++ src/App.js
@@ -1,52 +1,103 @@
 import { useState } from "react";
+import NFTCard from "./components/nftCard";
 import "./App.css";
 
 function App() {
   const [walletAddress, setWalletAddress] = useState("");
   const [collectionAddress, setCollectionAddress] = useState("");
   const [NFTs, setNFTs] = useState([]);
+  const [fetchForCollection, setFetchForCollection] = useState(false);
 
   const fetchNfts = async () => {
     let nfts;
-    if (!collectionAddress) {
-      nfts = await fetch(
-        `https://polygon-mumbai.g.alchemy.com/v2/-EDYQExUBgWlRHZmtwLOngaVutsR9825/getNFTs?owner=${walletAddress}`
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+    if (!collectionAddress.length) {
+      const baseURL =
+        "https://eth-mainnet.alchemyapi.io/v2/-EDYQExUBgWlRHZmtwLOngaVutsR9825/getNFTs/";
+      const fetchURL = `${baseURL}?owner=${walletAddress}&contractAddress=${collectionAddress}`;
+      nfts = await fetch(fetchURL, requestOptions).then((response) =>
+        response.json()
       );
     } else {
-      nfts = await fetch(
-        `https://polygon-mumbai.g.alchemy.com/v2/-EDYQExUBgWlRHZmtwLOngaVutsR9825/getNFTs?owner=${walletAddress}&contractAddress=${collectionAddress}`
+      const baseURL =
+        "https://eth-mainnet.alchemyapi.io/v2/-EDYQExUBgWlRHZmtwLOngaVutsR9825/getNFTs/";
+      const fetchURL = `${baseURL}?owner=${walletAddress}&contractAddresses%5B%5D=${collectionAddress}`;
+      nfts = await fetch(fetchURL, requestOptions).then((response) =>
+        response.json()
       );
     }
-    if (nfts && nfts.length) {
-      setNFTs(nfts);
+    if (nfts && nfts.ownedNfts) {
+      setNFTs(nfts.ownedNfts);
     }
-    console.log(nfts);
+  };
+
+  const fetchNFTsForCollection = async () => {
+    if (collectionAddress.length) {
+      var requestOptions = {
+        method: "GET",
+      };
+      const baseURL =
+        "https://eth-mainnet.alchemyapi.io/v2/-EDYQExUBgWlRHZmtwLOngaVutsR9825/getNFTsForCollection/";
+      const fetchURL = `${baseURL}?contractAddress=${collectionAddress}&withMetadata=${"true"}`;
+      const nfts = await fetch(fetchURL, requestOptions).then((data) =>
+        data.json()
+      );
+      if (nfts) {
+        setNFTs(nfts.nfts);
+      }
+    }
   };
 
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
+      <h1 className="font-bold text-3xl m-5 italic text-slate-700">
+        NFT Gallery
+      </h1>
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
         <input
+          disabled={fetchForCollection}
           type="text"
+          className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
           placeholder="Add the Wallet Address"
           onChange={(e) => setWalletAddress(e.target.value)}
         />
         <input
           type="text"
+          className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
           placeholder="Add the Collection Address"
           onChange={(e) => setCollectionAddress(e.target.value)}
         />
         <label className="text-gray-600 ">
-          <input type={"checkbox"} className="mr-2"></input>Fetch for collection
+          <input
+            type={"checkbox"}
+            className="mr-2"
+            placeholder={""}
+            onChange={(e) => {
+              setFetchForCollection(e.target.checked);
+            }}
+          />
+          Fetch for collection
         </label>
         <button
           className={
             "disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"
           }
-          onClick={fetchNfts}
+          onClick={() =>
+            fetchForCollection ? fetchNFTsForCollection() : fetchNfts()
+          }
         >
           Let's go!{" "}
         </button>
+      </div>
+      <div className="flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center">
+        {NFTs.length
+          ? NFTs.map((nft) => {
+              return <NFTCard nft={nft}></NFTCard>;
+            })
+          : null}
       </div>
     </div>
   );
---
+++ src/components/nftCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NFTCard = ({ nft }) => {
+  return <div>nftCard</div>;
+};
+
+export default NFTCard;
